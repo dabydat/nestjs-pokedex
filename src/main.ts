3305fcccaa93457ac71b213a51196d677cb9e300
David Gómez <alejandrogomez2510@gmail.com>
2024-01-22 12:39:00 -0400

    Added: Serving static content. Mongo connection. docker-compose for mongo. Readme file updated. Global prefix. Pokemon Resource

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Prefijo que se antepone a cada ruta de forma global
+  app.setGlobalPrefix('api/v2')
+
   await app.listen(3000);
 }
 bootstrap();
